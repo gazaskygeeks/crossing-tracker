f3b0551a0beb0e0bbe69cbef04d741eb21d63c03
4daliaomry4 <alaakhattab92@gmail.com>
2017-04-25 13:00:25 +0300

    moving the migration to the index file, adding hash fun, add validation folder for joi & using NODE_ENV from heroku to diffrensiate between production and developmint enviroments

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const server = require('./backend/server.js');
-server.start((err) => {
+const dbutils = require('./database/dbutils.js');
+
+dbutils.runMigrate((err) => {
   if (err) {
     throw err
   }
-  // eslint-disable-next-line no-console
-  console.log('Server running at:' + server.info.uri)
-})
+  // client.end()
+
+
+  server.start((err) => {
+    if (err) {
+      throw err
+    }
+    // eslint-disable-next-line no-console
+    console.log('Server running at:' + server.info.uri)
+  })
+});
