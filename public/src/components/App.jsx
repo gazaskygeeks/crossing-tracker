32e3ad94ce32efa69e3dd50faa3c55c1dc7935e2
akram <akram.badah@gmail.com>
2017-05-03 22:40:18 +0300

    removing the header from the login and the signup page

--- public/src/components/App.jsx
+++ public/src/components/App.jsx
@@ -1,11 +1,12 @@
 // This component handles the App template used on every page.
 import React,{PropTypes} from 'react';
 import Header from './templates/header.jsx';
+import {connect} from 'react-redux';
 class App extends React.Component {
   render() {
     return (
       <div>
-          <Header />
+          <Header logedIn={this.props.userLoged} />
           {this.props.children}
       </div>
     );
@@ -14,4 +15,13 @@ class App extends React.Component {
 App.propTypes = {
   children: PropTypes.object.isRequired
 };
-export default App;
+
+const mapStateToProps = (store) => {
+  return {userLoged: store.signin}
+}
+
+const app = connect(
+ mapStateToProps
+)(App)
+
+export default app;
