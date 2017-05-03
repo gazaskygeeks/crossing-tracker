d25dc1559b04222bfe245f8d5b620cfb6566f447
Mohammed Naji <moha.buy@gmail.com>
2017-05-03 21:59:16 +0300

    fix database query

--- database/orgsHelpers.js
+++ database/orgsHelpers.js
@@ -1,7 +1,7 @@
 const dbutils = require('./dbutils.js');
 
 function getOrgs(cb) {
-  const query = 'SELECT * From org';
+  const query = 'SELECT org_id,org_name From org';
   dbutils.runQuery(query, cb);
 }
 
