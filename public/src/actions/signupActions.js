9ebc929e6a6515e106d5d8139f989d3973d764ad
akram <akram.badah@gmail.com>
2017-05-08 14:10:35 +0300

    fixing the login and displaying the error message

---
+++ public/src/actions/signupActions.js
@@ -0,0 +1,26 @@
+/* eslint-disable */
+import * as types from './actionTypes';
+import store from '../store/store';
+import { hashHistory } from 'react-router';
+const register = (data)=>{
+  fetch('/signup',{
+    method: 'POST',
+    body:JSON.stringify(data),
+    credentials: 'include'
+  })
+  .then((response)=>{
+    return  response.json()
+  })
+.then((response)=>{
+    if(response.statusCode === 200 && response.msg === 'User regestered'){
+    hashHistory.push('success');
+  }else if(response.statusCode === 409 && response.msg === 'User already registered'){
+    alert(response.msg)
+  }else{
+    alert('invalid data ')
+  }
+}).catch((error) =>{
+  store.dispatch({type: types.REGISTER_USER_FAIL})
+})}
+
+export default register;
