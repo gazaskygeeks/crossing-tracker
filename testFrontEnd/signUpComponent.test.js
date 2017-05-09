5c0838aa29ea58f61d7ebab0fcbddce9fbd4f8f6
4daliaomry4 <alaakhattab92@gmail.com>
2017-05-09 15:02:20 +0300

    fix jest test in travis

--- testFrontEnd/signUpComponent.test.js
+++ testFrontEnd/signUpComponent.test.js
@@ -7,16 +7,16 @@ import {SignupPage} from '../public/src/components/signup/signupPage.jsx'
 import SelectOrgs from '../public/src/components/signup/selectOrgs.jsx'
 
 it('should render <SignupPage /> component   with <h1>Sign up</h1>', () => {
-    const wrapper = shallow(<SignupPage />);
+    const wrapper = shallow(<SignupPage signup={{statusCode : 400}} />);
     expect(wrapper.contains(<h1>Sign up</h1>)).toEqual(true);
   });
 
   it('should render  css style components', () => {
-    const wrapper = shallow(<SignupPage />);
+    const wrapper = shallow(<SignupPage signup={{statusCode : 409}}/>);
     expect(wrapper.find('.form-group')).toHaveLength(4);
   });
 
   it('should render  <SelectOrgs /> component', () => {
-    const wrapper = shallow(<SignupPage />);
+    const wrapper = shallow(<SignupPage signup={{statusCode : 200}} />);
     expect(wrapper.find(SelectOrgs)).toHaveLength(1);
   });
