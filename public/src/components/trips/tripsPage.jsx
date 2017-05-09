cca37ae1219f9bab446084d2b7a4ec41450c73f0
4daliaomry4 <alaakhattab92@gmail.com>
2017-05-10 00:58:30 +0300

    disply all joind trips

--- public/src/components/trips/tripsPage.jsx
+++ public/src/components/trips/tripsPage.jsx
@@ -89,9 +89,6 @@ class TripsPage extends React.Component {
 }
 
 const mapStateToProps = (store) => {
-  console.log('store.userTrips.createdTrip: ',store.userTrips.createdTrip);
-  console.log('store.userTrips.joinedTrip: ',store.userTrips.joinedTrip);
-  console.log('store.tripDetails[0]: ',store.tripDetails[0]);
   return {
     GetUserTrips: store.userTrips.createdTrip,
     UserJoinedTrips: store.userTrips.joinedTrip,
