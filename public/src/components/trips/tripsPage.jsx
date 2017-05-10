b1d3d2408d1ba7ffa5b1b56752e2f2c8197fa74e
akram <akram.badah@gmail.com>
2017-05-10 08:42:28 +0300

    redoing the edit trip in the frontend part

--- public/src/components/trips/tripsPage.jsx
+++ public/src/components/trips/tripsPage.jsx
@@ -1,67 +1,16 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import {getUserTrips} from '../../actions/myTripsActions.js';
-import {UpdateUserTrips} from '../../actions/myTripsActions.js';
 import UserTripsSection from './userTripsSection.jsx';
 import UserJoinedTrips from './userJoinedTrips.jsx';
-import viewTrip from '../../actions/tripDetailsActions.js';
 
 class TripsPage extends React.Component {
   constructor(props) {
     super(props);
-    if(!this.props.GetTripByID){
-      this.state = {
-        trip_id: 0,
-        tripdate: '',
-        time: '',
-        location_from: '',
-        location_to: '',
-        passing_by:'',
-        pass_point_time:'',
-        available_seats: 0
-      }
-    }else{
-      this.state = {
-        trip_id: this.props.GetTripByID.trip_id,
-        tripdate: this.props.GetTripByID.tripdate,
-        time: this.props.GetTripByID.time,
-        location_from: this.props.GetTripByID.location_from,
-        location_to: this.props.GetTripByID.location_to,
-        passing_by:this.props.GetTripByID.passing_by,
-        pass_point_time:this.props.GetTripByID.pass_point_time,
-        available_seats: this.props.GetTripByID.available_seats
-      }
-    }
   }
+
   componentWillMount(){
     this.props.UserTrips();
-  }
-  changeTime(ev) {
-    this.setState({time: ev.target.value});
-  }
-
-  changeTripDate(ev) {
-    this.setState({tripdate: ev.target.value});
-  }
-
-  changeLocationFrom(ev) {
-    this.setState({location_from: ev.target.value});
-  }
-
-  changeLocationTo(ev) {
-    this.setState({location_to: ev.target.value});
-  }
-
-  changePassingBy(ev) {
-    this.setState({passing_by: ev.target.value});
-  }
-
-  changePassingPointTime(ev) {
-    this.setState({pass_point_time: ev.target.value});
-  }
-
-  changeSeatsAvailable(ev) {
-    this.setState({available_seats: ev.target.value});
   }
 
   render() {
@@ -69,45 +18,26 @@ class TripsPage extends React.Component {
       <div>
         <UserTripsSection
           userTrips={this.props.GetUserTrips}
-          getUserTrips={() => this.props.UserTrips()}
-          getTrip = {id =>this.props.GetTrip(id)}
-          TripData = {this.props.GetTripByID}
-          rowState = {this.state}
-          changeTime = {this.changeTime.bind(this)}
-          changeTripDate = {this.changeTripDate.bind(this)}
-          changeLocationFrom = {this.changeLocationFrom.bind(this)}
-          changeLocationTo = {this.changeLocationTo.bind(this)}
-          changePassingBy = {this.changePassingBy.bind(this)}
-          changePassingPointTime = {this.changePassingPointTime.bind(this)}
-          changeSeatsAvailable = {this.changeSeatsAvailable.bind(this)}
-          UpdateTrip = {data => this.props.UpdateTrip(data)}
           />
-        <UserJoinedTrips joinedTrips={this.props.UserJoinedTrips}/>
+        <UserJoinedTrips
+          joinedTrips={this.props.UserJoinedTrips}
+          />
       </div>
     );
   }
 }
 
 const mapStateToProps = (store) => {
-  console.log('store.userTrips.createdTrip: ',store.userTrips.createdTrip);
-  console.log('store.userTrips.joinedTrip: ',store.userTrips.joinedTrip);
-  console.log('store.tripDetails[0]: ',store.tripDetails[0]);
+  console.log('store.userTrips: ',store.userTrips);
   return {
     GetUserTrips: store.userTrips.createdTrip,
     UserJoinedTrips: store.userTrips.joinedTrip,
-    GetTripByID: store.tripDetails[0]
   }
 }
 const mapDispatchToProps = () => {
   return {
     UserTrips: () => {
       getUserTrips()
-    },
-    GetTrip: (id) => {
-      viewTrip(id)
-    },
-    UpdateTrip: (data) => {
-      UpdateUserTrips(data)
     }
   }
 }
---
+++ public/src/components/trips/updateTripRow.jsx
@@ -0,0 +1,145 @@
+import React from 'react';
+import SelectLocations from './locationsOptions.jsx';
+import { hashHistory } from 'react-router';
+const UpdateTrip = (
+  {
+    userTrip,
+    Update,
+    changeTripDate,
+    changeTime,
+    changeLocationFrom,
+    changeLocationTo,
+    changePassingBy,
+    changePassingPointTime,
+    changeSeatsAvailable,
+    locations,
+    id,
+    getData
+  }
+) => {
+  if(!userTrip){
+
+    return <div>Loading...</div>;
+  }else{
+    console.log('userTrip: ',userTrip);
+  }
+
+  return(
+    <div key={id} className='col-md-offset-2 col-md-8'>
+      <ul>
+        <li>
+          <label>Trip date</label>
+          <span>
+            <input
+            type="date"
+            className="form-control"
+            value={userTrip.date}
+            onChange={changeTripDate}
+            />
+          </span>
+        </li>
+        <li>
+          <label>Time</label>
+          <span>
+            <input
+              type="time"
+              className="form-control"
+              value={userTrip.time}
+              onChange={changeTime}
+            />
+          </span>
+        </li>
+        <li>
+          <label>From</label>
+          <SelectLocations
+            label='From'
+            options={locations}
+            value={userTrip.location_from}
+            change={changeLocationFrom}
+            />
+        </li>
+        <li>
+          <label>To</label>
+          <SelectLocations
+            label='To'
+            options={locations}
+            value={userTrip.location_to}
+            change={changeLocationTo}
+            />
+        </li>
+        <li>
+          <label>Passing by</label>
+          <span>
+            <input
+              type="text"
+              className="form-control"
+              value={userTrip.passing_by}
+              onChange={changePassingBy}
+            />
+          </span>
+        </li>
+        <li>
+          <label>Passingpoint time</label>
+          <span>
+            <input
+              type="time"
+              className="form-control"
+              value={userTrip.pass_point_time}
+              onChange={changePassingPointTime}
+            />
+          </span>
+        </li>
+        <li>
+          <label>Seats available</label>
+          <span>
+            <input
+              type="number"
+              className="form-control"
+              value={userTrip.available_seats}
+              onChange={changeSeatsAvailable}
+            />
+          </span>
+        </li>
+      </ul>
+      <div className='btn-wrp-right'>
+        <button
+          className='btn btn-default'
+          onClick={() => hashHistory.push('trips')}
+          >
+            Cancel
+        </button>
+        <button
+          className='btn btn-default'
+          onClick={
+              () => {
+                return(
+                  getData(),
+                  hashHistory.push(`updatetrip/${id}`)
+                );
+              }
+          }
+          >
+            Get Trip's Old Data
+        </button>
+        <button
+          type='button'
+          className='btn btn-default'
+          onClick={
+            () => {
+              return(
+                Update(Object.assign(userTrip, {trip_id: id})),
+                hashHistory.push('trips')
+              );
+            }
+          }
+          >
+          Save Trip
+        </button>
+
+      </div>
+    </div>
+  );
+
+}
+
+export default UpdateTrip;
