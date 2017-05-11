02b89d12ec4a4868ce571bc808689769ddea5f7e
Mohammed Naji <moha.buy@gmail.com>
2017-05-11 11:25:58 +0300

    add participant notify function

--- backend/handlers/edittrip.js
+++ backend/handlers/edittrip.js
@@ -1,4 +1,6 @@
 const trip = require('../../database/tripHelpers')
+const user = require('../../database/userhelpers')
+const utiles = require('../utils.js');
 module.exports = (req, res) => {
   trip.getUserIdByTripId(req.payload.trip_id,(err,result)=>{
     if (err){
@@ -16,18 +18,54 @@ module.exports = (req, res) => {
             console.log('update trip error :',error)
             return res().code(500)
           }
+          trip.getusertripbytripid(req.payload.trip_id,(error,result)=>{
+            if (error)
+              {
+                // eslint-disable-next-line no-console
+              console.log('get user trip by trip id error :',error)
+              return res().code(500)
+            }
+            var rows =result.rows;
+            if (rows.length>0){
+              rows.map((elm)=>{
+
+                user.getuserbyid(elm.user_id,(error,result)=>{
+
+                  if (error)
+                    {
+                      // eslint-disable-next-line no-console
+                    console.log('get user by user id error :',error)
+                    return res().code(500)
+                  }
+                  var email=result.rows[0].email;
+                  var username=result.rows[0].username;
+                  utiles.sendemail(
+              'Site Admin <erezedule@gmail.com>',
+                    email,
+                    'There changes Happend in your trip',
+                    `Hi ${username},
+                    The owner of trip you joined,
+                    Made new Changes on trip,
+                    You can check changes by visiting
+                    MyTrip page
+                    `, (error, info) => {
+                      if (error) {
+                         // eslint-disable-next-line no-console
+                        console.log('sendemail :',error)
+                        return res().code(500)
+                      }
+                    })
+                })
+              })
+            }
+          })
           res({msg:'Your Trip Edit Successfully'});
         })
       }else{
-
         res({msg:'Not Allow to Edit this trip'});
       }
     }else{
-
-
       res({msg:'No Trip Found'});
     }
-
   })
-
 }
