3dda508e0f34967aeec5ebc3ec488965559a867d
4daliaomry4 <alaakhattab92@gmail.com>
2017-05-14 15:14:20 +0300

    disply trips in the moment you open the site

--- public/src/components/home/tripsSection.jsx
+++ public/src/components/home/tripsSection.jsx
@@ -6,7 +6,7 @@ const HomeTrips = (props) => {
   var trips;
   if (props.tripsList.length === 0) {
     trips = <tr>
-              <td colSpan={4}>No Available trip.
+              <td colSpan={4}>No Available trips.
                 You can create your own here:
                 <Link to={'/createtrip/'}>  Create Trip</Link>
               </td>
