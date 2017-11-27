79ba9e70ec479eaf702449b6bab3d71e3d36be91
Saral Kaushik <saral.kaushik@vincuventas.com>
2017-11-27 14:12:10 +0200

    update validaton

--- backend/validation.js
+++ backend/validation.js
@@ -8,7 +8,7 @@ const signupvalidation = {
 }
 const loginvalidation = {
   email: Joi.string().email().required(),
-  password: Joi.string().regex(/^[a-zA-Z0-9]{3,30}$/).min(6).max(20).required()
+  password: Joi.string().min(6).max(255).required(),
 }
 const tripvalidation = {
   tripdate: Joi.required(),
